8067f855fdd1f64643c976b496388fcc1276b9fb
benroywillis <benroywillis@gmail.com>
2023-07-24 14:33:46 -0700

    refactored build flow to build libGrammar.so in the root of the test directory, as to reduce redundant compilation work

--- Grammar/test/Buildflow.mk
+++ Grammar/test/Buildflow.mk
@@ -1,4 +1,6 @@
 ## compilation config that is required for each test
+# points to the root of the test folder
+TEST_ROOT=../
 # this variable dictates where the lib source files are, according to the test build folders
 TEST_TO_LIB=../../lib/
 # these are the grammar library source files, I can't yet get the lld linker to find all objects in libGrammar.so so for now static compilation has to be done
@@ -13,33 +15,37 @@ LLD=-fuse-ld=$(LLVM_INSTALL)bin/ld.lld -L$(CYCLEBITE_ROOT)build/lib/
 A_LINKS=$(SPDLOG_INSTALL)lib/libspdlog.a $(FMT_INSTALL)lib/libfmt.a $(LLVM_INSTALL)lib/*.a
 #D_LINKS=-lz -lpthread -l$(CYCLEBITE_ROOT)build/lib/libGraph.so -l$(CYCLEBITE_ROOT)build/lib/libGrammar.so
 #D_LINKS=-lz -lpthread -lGraph -lGrammar
-D_LINKS=-lz -lpthread -lGraph libGrammar.so
-INCLUDE=-I$(CYCLEBITE_ROOT)Graph/inc/ -I$(CYCLEBITE_ROOT)Grammar/lib/inc/ -I$(CYCLEBITE_ROOT) -I$(LLVM_INSTALL)include/ -I$(LLVM_INSTALL)include/llvm-c/ -I$(SPDLOG_INSTALL)include/ -I$(SPDLOG_INSTALL)../fmt_x64-linux/include/ -I$(NLOHMANNJSON_INSTALL)include/
+D_LINKS=-lz -lpthread -lGraph $(TEST_ROOT)libGrammar.so
+INCLUDE=-I$(CYCLEBITE_ROOT)Graph/inc/ -I$(CYCLEBITE_ROOT)Grammar/inc/ -I$(CYCLEBITE_ROOT)Grammar/lib/inc/ -I$(CYCLEBITE_ROOT) -I$(LLVM_INSTALL)include/ -I$(LLVM_INSTALL)include/llvm-c/ -I$(SPDLOG_INSTALL)include/ -I$(SPDLOG_INSTALL)../fmt_x64-linux/include/ -I$(NLOHMANNJSON_INSTALL)include/
 
 all : run
 
 # drives the compilation of each source file in the grammar library into an object
 GRAMMAR_SOURCES=$(wildcard $(TEST_TO_LIB)*.cpp)
 LOCAL_GRAMMAR_SOURCES=$(patsubst $(TEST_TO_LIB)%,%,$(GRAMMAR_SOURCES))
-LOCAL_GRAMMAR_OBJECTS=$(patsubst %.cpp,%.o,$(LOCAL_GRAMMAR_SOURCES))
+LOCAL_GRAMMAR_OBJECTS=$(patsubst %.cpp,$(TEST_ROOT)%.o,$(LOCAL_GRAMMAR_SOURCES))
 LOCAL_GRAMMAR_NAMES=$(patsubst %.cpp,%,$(LOCAL_GRAMMAR_SOURCES))
 define COMPILE_RULE = 
-$(1).o : $(TEST_TO_LIB)$(1).cpp
-	$(CXX) $(CXX_FLAGS) -c -fPIC $(INCLUDE) $(TEST_TO_LIB)$(1).cpp -o $(1).o
+$(TEST_ROOT)$(1).o : $(TEST_TO_LIB)$(1).cpp
+	$(CXX) $(CXX_FLAGS) -c -fPIC $(INCLUDE) $(TEST_TO_LIB)$(1).cpp -o $(TEST_ROOT)$(1).o
 endef
-objects : $(GRAMMAR_SOURCES)
+#objects : $(LOCAL_GRAMMAR_OBJECTS)
+$(LOCAL_GRAMMAR_OBJECTS) : 
 $(foreach f,$(LOCAL_GRAMMAR_NAMES), $(eval $(call COMPILE_RULE,$f)))
 
-libGrammar.so : objects
-	$(CXX) -shared $(LLD) -I$(CYCLEBITE_ROOT)Grammar/inc/ $(LOCAL_GRAMMAR_OBJECTS) $(INCLUDE) -o $@
+$(TEST_ROOT)libGrammar.so : $(LOCAL_GRAMMAR_OBJECTS)
+	$(CXX) -shared $(LLD) $(INCLUDE) $(LOCAL_GRAMMAR_OBJECTS) -o $@
 
-$(SOURCE).elf : $(SOURCE).cpp libGrammar.so
+$(SOURCE).elf : $(SOURCE).cpp $(TEST_ROOT)libGrammar.so
 	$(CXX) $(LLD) $(CXX_FLAGS) $(INCLUDE) $(D_LINKS) $(A_LINKS) $< -o $@
 
 run : $(SOURCE).elf
-	LD_LIBRARY_PATH="$(CYCLEBITE_ROOT)build/lib/:$(CYCLEBITE_ROOT)Grammar/test/IV/" ./$(SOURCE).elf -i $(CASE_PATH)instance_$(CASE_NAME).json -k $(CASE_PATH)kernel_$(CASE_NAME).json -b $(CASE_PATH)$(CASE_NAME).bc -bi $(CASE_PATH)BlockInfo_$(CASE_NAME).json -p $(CASE_PATH)/$(CASE_NAME).bin -o $(CASE_PATH)/kg_$(CASE_NAME).json
+	LD_LIBRARY_PATH="$(CYCLEBITE_ROOT)build/lib/:$(TEST_ROOT)" ./$(SOURCE).elf -i $(CASE_PATH)instance_$(CASE_NAME).json -k $(CASE_PATH)kernel_$(CASE_NAME).json -b $(CASE_PATH)$(CASE_NAME).bc -bi $(CASE_PATH)BlockInfo_$(CASE_NAME).json -p $(CASE_PATH)/$(CASE_NAME).bin -o $(CASE_PATH)/kg_$(CASE_NAME).json
 
 .PHONY:
 
 clean :
-	rm -rf *.elf
+	rm -rf *.elf *.o
+
+cleanall :
+	rm -rf *.elf *.o *.so
