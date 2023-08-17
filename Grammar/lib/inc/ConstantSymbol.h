6a9e8e8b4ffb99562ad298f3d0b96ceeaa9bf7bd
benroywillis <benroywillis@gmail.com>
2023-08-17 14:57:28 -0700

    generalized ConstantSymbol class to support any data type

--- Grammar/lib/inc/ConstantSymbol.h
+++ Grammar/lib/inc/ConstantSymbol.h
@@ -3,28 +3,17 @@
 
 namespace Cyclebite::Grammar
 {
-    enum class ConstantType
-    {
-        fp64,
-        fp32,
-        fp16,
-        int64_t,
-        uint64_t,
-        int32_t,
-        uint32_t,
-        int16_t,
-        uint16_t,
-        int8_t,
-        uint8_t,
-        int1_t
-    };
-
+    template <typename T>
     class ConstantSymbol : public Symbol
     {
     public:
-        ConstantSymbol(int64_t b) : Symbol("const") , bits(b) {}
+        ConstantSymbol(T b) : Symbol("const") , bits(b) {}
         std::string dump() const;
+        T getVal() const;
     private:
-        int64_t bits;
+        T bits;
     };
+    template class ConstantSymbol<int64_t>;
+    template class ConstantSymbol<float>;
+    template class ConstantSymbol<double>;
 } // namespace Cyclebite::Grammar
