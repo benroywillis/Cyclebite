821fde6f8a95190b9e070da86140777edee78e78
benroywillis <benroywillis@gmail.com>
2023-08-21 12:14:01 -0700

    simplified the way state instructions are found, and expanded the nodes labeled as state. Simplified the interface to color graph nodes.

--- Grammar/lib/inc/Categorize.h
+++ Grammar/lib/inc/Categorize.h
@@ -45,4 +45,5 @@ namespace Cyclebite::Grammar
     std::set<int64_t> findFunction(const std::map<std::string, std::set<llvm::BasicBlock *>> &kernelSets);
     std::set<int64_t> findState(const std::map<std::string, std::set<llvm::BasicBlock *>> &kernelSets);
     std::set<int64_t> findMemory(const std::map<std::string, std::set<llvm::BasicBlock *>> &kernelSets);
+    std::map<std::string, std::set<int64_t>> colorNodes( const std::map<std::string, std::set<llvm::BasicBlock *>> &kernelSets);
 } // namespace Cyclebite::Grammar
