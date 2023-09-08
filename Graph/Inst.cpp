a0b1ff9cde427d17ef71df75059f93f3791a4263
benroywillis <benroywillis@gmail.com>
2023-09-08 16:36:44 -0700

    some member functions to make Inst class more usable during DFG walks

--- Graph/Inst.cpp
+++ Graph/Inst.cpp
@@ -5,12 +5,18 @@ using namespace Cyclebite::Graph;
 
 Inst::Inst(const llvm::Instruction* inst, DNC t) : DataValue(inst), inst(inst), type(t)
 {
+    op = GetOp(inst->getOpcode());
     initOpToString();
 }
 
 const llvm::Instruction* Inst::getInst() const
 {
     return inst;
+}
+
+Operation Inst::getOp() const
+{
+    return op;
 }
 
 bool Inst::isTerminator() const
@@ -47,4 +53,68 @@ bool Inst::isMemory() const
 bool Inst::isFunction() const
 {
     return type == DNC::Function;
+}
+
+bool Inst::isBinaryOp() const
+{
+    if( op == Operation::add   ||
+        op == Operation::fadd  ||
+        op == Operation::sub   ||
+        op == Operation::fsub  ||
+        op == Operation::mul   ||
+        op == Operation::fmul  ||
+        op == Operation::fneg  ||
+        op == Operation::fdiv  ||
+        op == Operation::sdiv  ||
+        op == Operation::udiv  ||
+        op == Operation::urem  ||
+        op == Operation::srem  ||
+        op == Operation::frem  ||
+        op == Operation::gt    ||
+        op == Operation::gte   ||
+        op == Operation::lt    ||
+        op == Operation::lte   ||
+        op == Operation::sl    ||
+        op == Operation::sr    ||
+        op == Operation::asr   ||
+        op == Operation::andop ||
+        op == Operation::orop  ||
+        op == Operation::xorop )
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Inst::isCastOp() const
+{
+    if( op == Operation::trunc ||
+        op == Operation::zext ||
+        op == Operation::sext ||
+        op == Operation::fptoui ||
+        op == Operation::fptosi ||
+        op == Operation::uitofp ||
+        op == Operation::sitofp ||
+        op == Operation::fptrunc ||
+        op == Operation::fpext ||
+        op == Operation::ptrtoint ||
+        op == Operation::inttoptr ||
+        op == Operation::bitcast ||
+        op == Operation::addrspacecast)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Inst::isComparator() const
+{
+    if( op == Operation::icmp ||
+        op == Operation::fcmp ||
+        op == Operation::phi  ||
+        op == Operation::select )
+    {
+        return true;
+    }
+    return false;
 }
--- Graph/inc/Inst.h
+++ Graph/inc/Inst.h
@@ -18,17 +18,21 @@ namespace Cyclebite::Graph
     class Inst : public DataValue
     {
     public:
-        Operation op;
         std::shared_ptr<class ControlBlock> parent;
         Inst(const llvm::Instruction* inst, DNC t = DNC::None);
-        const llvm::Instruction* getInst() const; 
+        const llvm::Instruction* getInst() const;
+        Operation getOp() const; 
         bool isState() const;
         bool isFunction() const;
         bool isMemory() const;
         bool isTerminator() const;
         bool isCaller() const;
+        bool isBinaryOp() const;
+        bool isCastOp() const;
+        bool isComparator() const;
     private:
         const llvm::Instruction* inst;
         DNC type;
+        Operation op;
     };
 } // namespace Cyclebite::Graph
