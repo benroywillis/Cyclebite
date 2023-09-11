b5140f50194480d73e18c615f17663db3f87177c
benroywillis <benroywillis@gmail.com>
2023-09-11 09:15:21 -0700

    generalized collection construction to support idxVar input sets that are not a hierarchy

--- Grammar/lib/Collection.cpp
+++ Grammar/lib/Collection.cpp
@@ -10,28 +10,35 @@ using namespace std;
 Collection::Collection(const std::set<std::shared_ptr<IndexVariable>>& v, const std::shared_ptr<BasePointer>& p ) :  Symbol("collection"), bp(p)
 {
     // vars go in hierarchical order (parent-most is the first entry, child-most is the last entry)
-    shared_ptr<IndexVariable> parentMost = nullptr;
-    for( const auto& idx : v )
+    if( v.size() > 1 )
     {
-        if( idx->getParent() == nullptr )
+        shared_ptr<IndexVariable> parentMost = nullptr;
+        for( const auto& idx : v )
         {
-            if( parentMost )
+            if( idx->getParent() == nullptr )
             {
-                throw AtlasException("Found more than one parent-most index variables in this collection!");
+                if( parentMost )
+                {
+                    throw AtlasException("Found more than one parent-most index variables in this collection!");
+                }
+                parentMost = idx;
             }
-            parentMost = idx;
+        }
+        deque<shared_ptr<IndexVariable>> Q;
+        Q.push_front(parentMost);
+        while( !Q.empty() )
+        {
+            vars.push_back(Q.front());
+            if( Q.front()->getChild() )
+            {
+                Q.push_back(Q.front()->getChild());
+            }
+            Q.pop_front();
         }
     }
-    deque<shared_ptr<IndexVariable>> Q;
-    Q.push_front(parentMost);
-    while( !Q.empty() )
+    else
     {
-        vars.push_back(Q.front());
-        if( Q.front()->getChild() )
-        {
-            Q.push_back(Q.front()->getChild());
-        }
-        Q.pop_front();
+        vars.push_back(*v.begin());
     }
 }
 
