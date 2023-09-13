990dd35d1f6575e010df1069f7bf19f0b584863e
benroywillis <benroywillis@gmail.com>
2023-09-13 07:44:16 -0700

    fixed bug in the constant function dump method

--- Grammar/lib/ConstantFunction.cpp
+++ Grammar/lib/ConstantFunction.cpp
@@ -15,19 +15,19 @@ string ConstantFunction::dump() const
             case llvm::Type::IntegerTyID:
                 switch( llvm::cast<llvm::IntegerType>(argi->getType())->getIntegerBitWidth() )
                 {
-                    case 1:  funcCall += "bool arg"+to_string(argIndex);
-                    case 8:  funcCall += "int8 arg"+to_string(argIndex);
-                    case 16: funcCall += "int16 arg"+to_string(argIndex);
-                    case 32: funcCall += "int32 arg"+to_string(argIndex);
-                    case 64: funcCall += "int64 arg"+to_string(argIndex);
-                    default: funcCall += "nan arg"+to_string(argIndex);
+                    case 1:  funcCall += "bool arg"+to_string(argIndex); break;
+                    case 8:  funcCall += "int8 arg"+to_string(argIndex); break;
+                    case 16: funcCall += "int16 arg"+to_string(argIndex); break;
+                    case 32: funcCall += "int32 arg"+to_string(argIndex); break;
+                    case 64: funcCall += "int64 arg"+to_string(argIndex); break;
+                    default: funcCall += "nan arg"+to_string(argIndex); break;
                 }
-            case llvm::Type::FloatTyID:    funcCall += "float arg"+to_string(argIndex);
-            case llvm::Type::DoubleTyID:   funcCall += "double arg"+to_string(argIndex);
-            case llvm::Type::PointerTyID:  funcCall += "pointer arg"+to_string(argIndex);
-            case llvm::Type::FunctionTyID: funcCall += "func arg"+to_string(argIndex);
-            case llvm::Type::StructTyID:   funcCall += "struct arg"+to_string(argIndex);
-            case llvm::Type::ArrayTyID:    funcCall += "array arg"+to_string(argIndex);
+            case llvm::Type::FloatTyID:    funcCall += "float arg"+to_string(argIndex); break;
+            case llvm::Type::DoubleTyID:   funcCall += "double arg"+to_string(argIndex); break;
+            case llvm::Type::PointerTyID:  funcCall += "pointer arg"+to_string(argIndex); break;
+            case llvm::Type::FunctionTyID: funcCall += "func arg"+to_string(argIndex); break;
+            case llvm::Type::StructTyID:   funcCall += "struct arg"+to_string(argIndex); break;
+            case llvm::Type::ArrayTyID:    funcCall += "array arg"+to_string(argIndex); break;
             default: funcCall += "arg"+to_string(argIndex);
         }
         if( argi != prev(f->arg_end()) )
