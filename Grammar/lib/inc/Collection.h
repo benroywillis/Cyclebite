720bb1646e1403e951ada1e38b1d5de0c95c3125
benroywillis <benroywillis@gmail.com>
2023-10-17 17:50:12 -0700

    some new documentation on the collection class

--- Grammar/lib/inc/Collection.h
+++ Grammar/lib/inc/Collection.h
@@ -12,7 +12,10 @@ namespace Cyclebite::Grammar
     class Collection : public Symbol
     {
     public:
-        Collection( const std::vector<std::shared_ptr<IndexVariable>>& v, const std::set<std::shared_ptr<BasePointer>>& p, const std::set<const llvm::Value*>& eps );
+        /// @param v    The group of index variables that offset the memory of this collection. They should define the access patterns of the collection.
+        /// @param p    The base pointers of this collection. Multiple base pointers can be used - e.g., one to offset with index variables, and one that is used by the index variable(s) themselves
+        /// @param e    The memory primitive(s) that are produced by this collection. This represents the loaded or stored values or places this collection facilitates.
+        Collection( const std::vector<std::shared_ptr<IndexVariable>>& v, const std::set<std::shared_ptr<BasePointer>>& p, const std::set<const llvm::Value*>& e );
         ~Collection() = default;
         uint32_t getNumDims() const;
         const std::shared_ptr<IndexVariable>& operator[](unsigned i) const;
