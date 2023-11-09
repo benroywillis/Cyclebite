435c744ac696d27906d383be037492494b4d8243
benroywillis <benroywillis@gmail.com>
2023-11-09 09:02:39 -0700

    added exception handling around cycle building method

--- Grammar/lib/Cycle.cpp
+++ Grammar/lib/Cycle.cpp
@@ -197,107 +197,115 @@ set<shared_ptr<Cycle>> Cyclebite::Grammar::ConstructCycles(const nlohmann::json&
 {
     map<string, shared_ptr<Cycle>> idToCycle;
     set<shared_ptr<Cycle>> cycles;
-    for (auto &[i, l] : kernelJson["Kernels"].items())
+    try
     {
-        // first, construct set of ControlBlock objects within this cycle
-        set<shared_ptr<ControlBlock>, p_GNCompare> blocks;
-        for( const auto& id : l["Blocks"].get<vector<int64_t>>() )
+        for (auto &[i, l] : kernelJson["Kernels"].items())
         {
-            blocks.insert( BBCBMap.at(IDToBlock.at(id)) );
-        }
-        // second, find the cmp insts that can either exit the cycle
-        set<const llvm::Instruction*> exiters;
-        for( const auto& b : blocks )
-        {
-            for( const auto& i : b->getInstructions() )
+            // first, construct set of ControlBlock objects within this cycle
+            set<shared_ptr<ControlBlock>, p_GNCompare> blocks;
+            for( const auto& id : l["Blocks"].get<vector<int64_t>>() )
             {
-                if( i->isTerminator() )
+                blocks.insert( BBCBMap.at(IDToBlock.at(id)) );
+            }
+            // second, find the cmp insts that can either exit the cycle
+            set<const llvm::Instruction*> exiters;
+            for( const auto& b : blocks )
+            {
+                for( const auto& i : b->getInstructions() )
                 {
-                    // for regular loops
-                    if( auto br = llvm::dyn_cast<llvm::BranchInst>(i->getInst()) )
+                    if( i->isTerminator() )
                     {
-                        // br must have at least two targets
-                        if( br->getNumSuccessors() > 1 )
+                        // for regular loops
+                        if( auto br = llvm::dyn_cast<llvm::BranchInst>(i->getInst()) )
                         {
-                            // get its targets and see if they exit the cycle block set
-                            // this check here is to make sure both targets are alive, if even one of them is dead we don't consider this br to be the iterator br
-                            if( BBCBMap.contains(br->getSuccessor(0)) && BBCBMap.contains(br->getSuccessor(1)) )
+                            // br must have at least two targets
+                            if( br->getNumSuccessors() > 1 )
                             {
-                                auto dest0 = BBCBMap.at( br->getSuccessor(0) );
-                                auto dest1 = BBCBMap.at( br->getSuccessor(1) );
-                                if( !blocks.contains(dest0) || !blocks.contains(dest1) )
+                                // get its targets and see if they exit the cycle block set
+                                // this check here is to make sure both targets are alive, if even one of them is dead we don't consider this br to be the iterator br
+                                if( BBCBMap.contains(br->getSuccessor(0)) && BBCBMap.contains(br->getSuccessor(1)) )
                                 {
-                                    // the destination of this edge is outside the cycle, thus it is an exit
-                                    exiters.insert(br);
+                                    auto dest0 = BBCBMap.at( br->getSuccessor(0) );
+                                    auto dest1 = BBCBMap.at( br->getSuccessor(1) );
+                                    if( !blocks.contains(dest0) || !blocks.contains(dest1) )
+                                    {
+                                        // the destination of this edge is outside the cycle, thus it is an exit
+                                        exiters.insert(br);
+                                    }
                                 }
                             }
                         }
-                    }
-                    else if( auto sel = llvm::dyn_cast<llvm::SelectInst>(i->getInst()) )
-                    {
-                        // do something
-                        throw CyclebiteException("Cannot yet support select instructions for cycle iteration conditions!");
-                    }
-                    else if( auto ret = llvm::dyn_cast<llvm::ReturnInst>(i->getInst()) )
-                    {
-                        // if there is a function within the task, it's probably not the boundary of they cycle
-                        // to confirm that this return exits the loop, we need to know what the successors of the block are
-                        // corner case: a shared function's return will have successors outside the task
-                        //  - for this reason we do away with the check (for now) 
-                        /*if( BBCBMap.find(ret->getParent()) != BBCBMap.end() )
+                        else if( auto sel = llvm::dyn_cast<llvm::SelectInst>(i->getInst()) )
                         {
-                            for( const auto& succ : BBCBMap.at(ret->getParent())->getSuccessors() )
+                            // do something
+                            throw CyclebiteException("Cannot yet support select instructions for cycle iteration conditions!");
+                        }
+                        else if( auto ret = llvm::dyn_cast<llvm::ReturnInst>(i->getInst()) )
+                        {
+                            // if there is a function within the task, it's probably not the boundary of they cycle
+                            // to confirm that this return exits the loop, we need to know what the successors of the block are
+                            // corner case: a shared function's return will have successors outside the task
+                            //  - for this reason we do away with the check (for now) 
+                            /*if( BBCBMap.find(ret->getParent()) != BBCBMap.end() )
                             {
-                                if( blocks.find( succ->getSnk() ) == blocks.end() )
+                                for( const auto& succ : BBCBMap.at(ret->getParent())->getSuccessors() )
                                 {
-                                    PrintVal(ret);
-                                    // we are in trouble. There' no way for us to know what the condition is that leads to this exit, thus we don't have a known method of finding the iteratorCondition of this cycle
-                                    throw CyclebiteException("Cannot yet support function return instructions when finding cycle iteration condition!");
+                                    if( blocks.find( succ->getSnk() ) == blocks.end() )
+                                    {
+                                        PrintVal(ret);
+                                        // we are in trouble. There' no way for us to know what the condition is that leads to this exit, thus we don't have a known method of finding the iteratorCondition of this cycle
+                                        throw CyclebiteException("Cannot yet support function return instructions when finding cycle iteration condition!");
+                                    }
                                 }
-                            }
-                        }*/
-                        continue;
+                            }*/
+                            continue;
+                        }
                     }
                 }
             }
-        }
-        //  now that we have the block set and the iterator cmp, construct the cycle object
-        if( !exiters.empty() )
-        {
-            auto newCycle = make_shared<Cycle>(exiters, blocks, stol(i));
-            idToCycle[string(i)] = newCycle;
-            // add this cycle to the parent cycles, if they exist
-            for( const auto& c : l["Children"] )
+            //  now that we have the block set and the iterator cmp, construct the cycle object
+            if( !exiters.empty() )
             {
-                string id = to_string(c.get<int64_t>());
-                if( idToCycle.find( id ) != idToCycle.end() )
+                auto newCycle = make_shared<Cycle>(exiters, blocks, stol(i));
+                idToCycle[string(i)] = newCycle;
+                // add this cycle to the parent cycles, if they exist
+                for( const auto& c : l["Children"] )
                 {
-                    auto child = idToCycle.find( id )->second;
-                    child->addParent(newCycle);
-                    newCycle->addChild(child);
+                    string id = to_string(c.get<int64_t>());
+                    if( idToCycle.find( id ) != idToCycle.end() )
+                    {
+                        auto child = idToCycle.find( id )->second;
+                        child->addParent(newCycle);
+                        newCycle->addChild(child);
+                    }
+                }
+                for( const auto& p : l["Parents"] )
+                {
+                    string id = to_string(p.get<int64_t>());
+                    if( idToCycle.find(id) != idToCycle.end() )
+                    {
+                        auto parent = idToCycle.find(id)->second;
+                        parent->addChild(newCycle);
+                        newCycle->addParent(parent);
+                    }
+                }
+                cycles.insert(newCycle);
+                // if it maps to a task, add it to the taskCycle set
+                if( instanceJson["Kernels"].find(string(i)) != instanceJson["Kernels"].end() )
+                {
+                    taskCycles.insert(newCycle);
                 }
             }
-            for( const auto& p : l["Parents"] )
+            else
             {
-                string id = to_string(p.get<int64_t>());
-                if( idToCycle.find(id) != idToCycle.end() )
-                {
-                    auto parent = idToCycle.find(id)->second;
-                    parent->addChild(newCycle);
-                    newCycle->addParent(parent);
-                }
-            }
-            cycles.insert(newCycle);
-            // if it maps to a task, add it to the taskCycle set
-            if( instanceJson["Kernels"].find(string(i)) != instanceJson["Kernels"].end() )
-            {
-                taskCycles.insert(newCycle);
+                throw CyclebiteException("Could not find iteratorCmp for a cycle!");
             }
         }
-        else
-        {
-            throw CyclebiteException("Could not find iteratorCmp for a cycle!");
-        }
+    }
+    catch( CyclebiteException& e )
+    {
+        spdlog::critical(e.what());
+        exit(1);
     }
     return cycles;
 }
