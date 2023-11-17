7fa8f4dd4473d2aa7dfa5fb9a3c39e4c6ffc9407
benroywillis <benroywillis@gmail.com>
2023-11-16 17:20:58 -0700

    dumped a bunch of old methods that were getting in the way

--- Grammar/lib/inc/BasePointer.h
+++ Grammar/lib/inc/BasePointer.h
@@ -23,20 +23,12 @@ namespace Cyclebite::Grammar
     class BasePointer : public Symbol
     {
     public:
-        BasePointer(const std::shared_ptr<Cyclebite::Graph::DataValue>& n, const std::vector<std::pair<const llvm::GetElementPtrInst*, const llvm::LoadInst*>>& lds, 
-                    const std::vector<std::pair<const llvm::GetElementPtrInst*, const llvm::StoreInst*>>& sts) : Symbol("bp"), node(n), loads(lds), stores(sts) {}
+        BasePointer(const std::shared_ptr<Cyclebite::Graph::DataValue>& n) : Symbol("bp"), node(n) {}
         ~BasePointer() = default;
         const std::shared_ptr<Cyclebite::Graph::DataValue>& getNode() const;
-        const std::vector<std::pair<const llvm::GetElementPtrInst*, const llvm::LoadInst*>>& getAccesses() const;
-        const std::vector<std::pair<const llvm::GetElementPtrInst*, const llvm::StoreInst*>>& getStores() const;
-        const std::vector<const llvm::LoadInst*> getlds() const;
-        const std::vector<const llvm::StoreInst*> getsts() const;
-        const std::vector<const llvm::GetElementPtrInst*> getgps() const;
         bool isOffset( const llvm::Value* val ) const;
     private:
         std::shared_ptr<Cyclebite::Graph::DataValue> node;
-        std::vector<std::pair<const llvm::GetElementPtrInst*, const llvm::LoadInst*>> loads;
-        std::vector<std::pair<const llvm::GetElementPtrInst*, const llvm::StoreInst*>> stores;
     };
     std::set<std::shared_ptr<BasePointer>> getBasePointers(const std::shared_ptr<Task>& t);
 } // namespace Cyclebite::Grammar
